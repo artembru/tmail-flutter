e738381af4bcbaa44e3d0eb73ee836f340a59e01
dab246 <tdvu@linagora.com>
2024-01-30 04:41:15 +0700

    TF-2475 Handle format style rich text in vacation
    Signed-off-by: dab246 <tdvu@linagora.com>
    (cherry picked from commit 2b707d9e3ee8ffb99a6efe7feb44f2905a556f82)

--- lib/features/manage_account/presentation/vacation/vacation_controller.dart
+++ lib/features/manage_account/presentation/vacation/vacation_controller.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jmap_dart_client/jmap/mail/vacation/vacation_response.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
-import 'package:rich_text_composer/richtext_controller.dart';
+import 'package:rich_text_composer/rich_text_composer.dart';
+import 'package:rich_text_composer/views/commons/constants.dart';
 import 'package:tmail_ui_user/features/base/base_controller.dart';
 import 'package:tmail_ui_user/features/composer/presentation/controller/rich_text_web_controller.dart';
 import 'package:tmail_ui_user/features/mailbox_creator/domain/model/verification/empty_name_validator.dart';
@@ -63,6 +64,7 @@ class VacationController extends BaseController {
   @override
   void onInit() {
     _initWorker();
+    _initFocusListener();
     super.onInit();
   }
 
@@ -88,6 +90,16 @@ class VacationController extends BaseController {
         currentVacation = vacation;
         final newVacationPresentation = currentVacation?.toVacationPresentation();
         _initializeValueForVacation(newVacationPresentation ?? VacationPresentation.initialize());
+      }
+    });
+  }
+
+  void _initFocusListener() {
+    subjectTextFocusNode.addListener(() {
+      if (subjectTextFocusNode.hasFocus == true) {
+        if (PlatformInfo.isMobile) {
+          richTextControllerForMobile.hideRichTextView();
+        }
       }
     });
   }
@@ -155,6 +167,11 @@ class VacationController extends BaseController {
   }
 
   void selectDate(BuildContext context, DateType dateType, DateTime? currentDate) async {
+    if (PlatformInfo.isMobile) {
+      richTextControllerForMobile.htmlEditorApi?.unfocus();
+    }
+    FocusScope.of(context).unfocus();
+
     _accountDashBoardController.isVacationDateDialogDisplayed = true;
     final datePicked = await showDatePicker(
         context: context,
@@ -196,6 +213,11 @@ class VacationController extends BaseController {
   }
 
   void selectTime(BuildContext context, DateType dateType, TimeOfDay? currentTime) async {
+    if (PlatformInfo.isMobile) {
+      richTextControllerForMobile.htmlEditorApi?.unfocus();
+    }
+    FocusScope.of(context).unfocus();
+
     _accountDashBoardController.isVacationDateDialogDisplayed = true;
     final timePicked = await showTimePicker(
       context: context,
@@ -339,7 +361,6 @@ class VacationController extends BaseController {
   void clearFocusEditor(BuildContext context) {
     if (PlatformInfo.isMobile) {
       richTextControllerForMobile.htmlEditorApi?.unfocus();
-      KeyboardUtils.hideSystemKeyboardMobile();
     }
     KeyboardUtils.hideKeyboard(context);
   }
@@ -349,7 +370,22 @@ class VacationController extends BaseController {
     _settingController.backToUniversalSettings();
   }
 
-  void onFocusHTMLEditor() async {
+  void initRichTextForMobile(BuildContext context, HtmlEditorApi editorApi) {
+    richTextControllerForMobile.onCreateHTMLEditor(
+      editorApi,
+      onFocus: () => onFocusHTMLEditor(context),
+      onEnterKeyDown: onEnterKeyDown,
+    );
+  }
+
+  void onFocusHTMLEditor(BuildContext context) async {
+    if (PlatformInfo.isAndroid) {
+      FocusScope.of(context).unfocus();
+      await Future.delayed(
+        const Duration(milliseconds: 300),
+        richTextControllerForMobile.showDeviceKeyboard);
+    }
+
     subjectTextFocusNode.unfocus();
 
     await Scrollable.ensureVisible(htmlKey.currentContext!);
